476a36a4be348a36b1e54ecded28d17b49e47fda
Hashim34643 <hashimhussain7312@gmail.com>
2024-02-08 16:13:09 +0000

    sortQueries implemented

--- src/Components/ArticlesListAllArticles.jsx
+++ src/Components/ArticlesListAllArticles.jsx
@@ -1,49 +1,67 @@
-import { useState, useEffect } from 'react';
+import React, { useState, useEffect } from 'react';
 import axios from 'axios';
 import { Link } from 'react-router-dom';
+import { format } from "date-fns";
+import SortQueries from './SortQueries';
 import "../Components-Styles/ArticlesList.css"
-import {format} from "date-fns";
 
-
-const ArticlesListAllArticles = () => {
+const ArticleListAllArticles = () => {
   const [articles, setArticles] = useState([]);
+  const [sortBy, setSortBy] = useState("created_at");
+  const [sortOrder, setSortOrder] = useState("desc");
+
   const formatDate = (dateString) => {
     return format(new Date(dateString), 'MMMM dd, yyyy HH:mm');
   };
+
+  const fetchArticles = () => {
+    axios.get(`https://backend-nc-news-l5zm.onrender.com/api/articles?sort_by=${sortBy}&order=${sortOrder}&limit=100000`)
+      .then(response => {
+        setArticles(response.data.articles);
+      })
+      .catch(error => {
+        console.error("error", error);
+      });
+  };
+
   useEffect(() => {
-    const fetchArticles = () => {
-      axios.get(`https://backend-nc-news-l5zm.onrender.com/api/articles?limit=100000`)
-        .then(response => {
-          setArticles(response.data.articles);
-        })
-        .catch(error => {
-          console.log("error", error);
-        });
-    };
     fetchArticles();
-  }, []);
+  }, [sortBy, sortOrder]);
+
+  const handleSortByChange = (sortBy) => {
+    setSortBy(sortBy);
+  };
+
+  const handleSortOrderChange = (sortOrder) => {
+    setSortOrder(sortOrder);
+  };
 
   return (
     <>
-    <div className='articless-list-all-articles'>
-      <h2>Latest Articles</h2>
-      <ul>
-        <div className="single-article-all-article">
-        {articles.map(article => (
-          <li key={article.title}>
-            <Link to={`/article/${article.article_id}`}>
-              <h3>{article.title}</h3>
-            </Link>
-              <p>{article.topic}</p>
-              <p>{article.author}</p>
-              <p>{formatDate(article.created_at)}</p>
-          </li>
-        ))}
-        </div>
-      </ul>
+      <SortQueries
+        searchParams={location.search}
+        handleSortByChange={handleSortByChange}
+        handleSortOrderChange={handleSortOrderChange}
+      />
+      <div className='articless-list-all-articles'>
+        <h2>Latest Articles</h2>
+        <ul>
+          <div className="single-article-all-article">
+            {articles.map(article => (
+              <li key={article.title}>
+                <Link to={`/article/${article.article_id}`}>
+                  <h3>{article.title}</h3>
+                </Link>
+                <p>{article.topic}</p>
+                <p>{article.author}</p>
+                <p>{formatDate(article.created_at)}</p>
+              </li>
+            ))}
+          </div>
+        </ul>
       </div>
-      </>
+    </>
   );
 }
 
-export default ArticlesListAllArticles;
+export default ArticleListAllArticles;
